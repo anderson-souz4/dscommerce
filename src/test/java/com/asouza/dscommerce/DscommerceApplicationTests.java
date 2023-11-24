95c39678bec1c0dbe67c083946c825831240c8b7
exper <andersondesouzalopes69@gmail.com>
2023-11-24 02:26:34 -0300

    Perfil de test, modelagem das entidades Order e User, enum OrderStatus.

---
+++ src/test/java/com/asouza/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asouza.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
